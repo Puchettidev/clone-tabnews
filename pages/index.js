64ee2486563516b4148b6b2d5bbbb7f1bff10d0c
João Victor Moreira Puchetti <jvpuchetti@gmail.com>
2026-06-29 11:06:43 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home () {
-    return <h1> Duda eu amo você. se você me ama, dá uma risadinha! adicionar informação</h1>
-
+function Home() {
+  return (
+    <h1>
+      {" "}
+      Duda eu amo você. se você me ama, dá uma risadinha! adicionar informação
+    </h1>
+  );
 }
 
 export default Home;
